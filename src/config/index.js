7ed968b6b5c5f336e0d3dc8107597f8c05cd9fae
Taylor <taylormisch@gmail.com>
2021-03-01 07:30:52 -0600

    Compile issues

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,13 @@
 const dotenv = require("dotenv");
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+// process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 // config() will read the .env file, parse the contents, assign it to process.env
 const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+// if (envFound.error) {
+//   // This error should crash whole process
+//   throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 
 export default {
   port: process.env.PORT,
